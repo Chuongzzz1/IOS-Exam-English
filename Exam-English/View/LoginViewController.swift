40633075394a7502c7c7543260858a4dd1a14dac
Chuongzzz1 <tranvanchuong1412@gmail.com>
2024-04-15 00:13:24 +0700

    #1-LoginUI

---
+++ Exam-English/View/LoginViewController.swift
@@ -0,0 +1,16 @@
+//
+//  LoginViewController.swift
+//  Exam-English
+//
+//  Created by Trần Văn Chương on 14/04/2024.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+}
